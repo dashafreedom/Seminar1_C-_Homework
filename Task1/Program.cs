bce4a8994080dad55df7ce928b8ec443318908fe
DaryaLukyanova <dasha.opt@mail.ru>
2022-11-15 16:03:22 +0500

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите число1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+int min = b;
+int max1 = b;
+int min1 = a;
+
+if(a > b)
+{
+    Console.WriteLine("max = " + max);
+    Console.WriteLine("min = " + min);
+}
+else
+{
+    Console.WriteLine("max = " + max1);
+    Console.WriteLine("min = " + min1);
+}
