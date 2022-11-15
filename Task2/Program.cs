2eef8660765e2ff7d1f1f7596c3a079044fdcb7d
DaryaLukyanova <dasha.opt@mail.ru>
2022-11-15 16:54:55 +0500

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число 1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число 2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число 3: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.WriteLine("Максимальное число равно " + max);
