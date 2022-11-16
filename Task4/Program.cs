674a441d2731ac2c80409457e3f0d35dd1f374c7
DaryaLukyanova <dasha.opt@mail.ru>
2022-11-16 19:36:14 +0500

    Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int beginN = 1;
+
+while(beginN <= N)
+{
+    if(beginN % 2 == 0)
+    {
+        Console.Write(beginN + " ");
+    } 
+
+    beginN++;
+}
